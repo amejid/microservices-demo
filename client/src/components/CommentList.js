700091267cf08d36fcc17ba4776a00ad606b53c1
amejid <abdelmejid89@gmail.com>
2023-02-16 21:19:07 +0300

    Integrate the comment moderation service with other services

--- client/src/components/CommentList.js
+++ client/src/components/CommentList.js
@@ -1,9 +1,19 @@
 const CommentList = ({ comments }) => {
-  const renderedComments = comments.map((comment) => (
-    <li key={comment.id}>
-      <h5>{comment.content}</h5>
-    </li>
-  ));
+  const renderedComments = comments.map((comment) => {
+    let content;
+
+    if (comment.status === 'approved') {
+      content = comment.content;
+    }
+    if (comment.status === 'pending') {
+      content = 'Awaiting moderation';
+    }
+    if (comment.status === 'rejected') {
+      content = 'Rejected';
+    }
+
+    return <li key={comment.id}>{content}</li>;
+  });
 
   return <ul>{renderedComments}</ul>;
 };
--- query/index.js
+++ query/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const axios = require('axios');
 
 const app = express();
 app.use(cors());
@@ -7,26 +8,48 @@ app.use(express.json());
 
 const posts = {};
 
-app.get('/posts', (req, res) => {
-  res.send(posts);
-});
-
-app.post('/events', (req, res) => {
-  const { type, data } = req.body;
-
+const handleEvent = (type, data) => {
   if (type === 'PostCreated') {
     const { id, title } = data;
     posts[id] = { id, title, comments: [] };
   }
 
   if (type === 'CommentCreated') {
-    const { id, content, postId } = data;
+    const { id, content, postId, status } = data;
 
     const post = posts[postId];
-    post.comments.push({ id, content });
+    post.comments.push({ id, content, status });
   }
+
+  if (type === 'CommentUpdated') {
+    const { id, content, postId, status } = data;
+
+    const post = posts[postId];
+    const comment = post.comments.find((comment) => comment.id === id);
+
+    comment.status = status;
+    comment.content = content;
+  }
+};
+
+app.get('/posts', (req, res) => {
+  res.send(posts);
+});
+
+app.post('/events', (req, res) => {
+  const { type, data } = req.body;
+  handleEvent(type, data);
 
   res.send({});
 });
 
-app.listen(4002, () => console.log('Listening on 4002'));
+app.listen(4002, async () => {
+  console.log('Listening on 4002');
+  const { data } = await axios.get('http://localhost:4005/events');
+
+  for (let event of data) {
+    console.log('Processing event: ', event.type);
+
+    handleEvent(event.type, event.data);
+  }
+});
